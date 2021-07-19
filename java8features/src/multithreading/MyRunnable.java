fa4c3a601211b32a3c1128a30154044e1aa75b7c
emugi01@safeway.com <Ernest.Mugisha@albertsons.com>
2021-07-18 22:59:32 -0500

    multi threading

---
+++ java8features/src/multithreading/MyRunnable.java
@@ -0,0 +1,24 @@
+package multithreading;
+
+public class MyRunnable {
+
+    public static void main(String[] args) {
+
+        Runnable r=()->{
+            for(int i=0;i<10;i++)
+            {
+                System.out.println("child Thread...");
+            }
+
+        };
+        Thread t=new Thread(r);
+        t.start();
+
+        for (int i=0;i<10;i++)
+        {
+            System.out.println("Main Thread...");
+        }
+    }
+
+
+}
